cb9c17b98f7ffb1bbc7f722aee6f683a5bb54da3
dmswo106 <dmswo106@naver.com>
2022-02-19 14:13:59 +0900

    1.메소드 이름으로 쿼리생성 2.NamedQuery(실무에서 잘 사용안함 -> 하지만 장점은 있음)

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -56,5 +56,34 @@ public class MemberRepositoryTest {
 
         long deleteCount = memberRepository.count();
         Assertions.assertThat(deleteCount).isEqualTo(0);
+    }
 
-    }}
+    @Test
+    public void findByUsernameAndAgeGreaterThen() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+        Assertions.assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        Assertions.assertThat(result.get(0).getAge()).isEqualTo(20);
+    }
+
+    @Test
+    public void findHelloBy() {
+        memberRepository.findTop3HelloBy();
+    }
+
+    @Test
+    public void testNamedQuery() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsername("AAA");
+        Member findMember = result.get(0);
+        Assertions.assertThat(findMember).isEqualTo(m1);
+    }
+}
