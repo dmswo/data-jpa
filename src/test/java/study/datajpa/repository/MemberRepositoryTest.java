5c77a9e24e3ebda452a171a549d5ca1c2ebeee4b
dmswo106 <dmswo106@naver.com>
2022-02-19 13:29:35 +0900

    공통인터페이스 적용

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -9,21 +9,20 @@ import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
 import java.util.List;
-import java.util.Optional;
 
 @SpringBootTest
 @Transactional
 @Rollback(value = false)
 public class MemberRepositoryTest {
 
-    @Autowired MemberJpaRepository memberJpaRepository;
+    @Autowired MemberRepository memberRepository;
 
     @Test
     public void testMember() {
         Member member = new Member("memberA");
-        Member savedMember = memberJpaRepository.save(member);
+        Member savedMember = memberRepository.save(member);
 
-        Member findMember = memberJpaRepository.findById(savedMember.getId()).get();
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
 
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
@@ -34,28 +33,28 @@ public class MemberRepositoryTest {
     public void basicCRUD() {
         Member member1 = new Member("member1");
         Member member2 = new Member("member2");
-        memberJpaRepository.save(member1);
-        memberJpaRepository.save(member2);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
 
         //단건 조회 검증
-        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
-        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
         Assertions.assertThat(findMember1).isEqualTo(member1);
         Assertions.assertThat(findMember2).isEqualTo(member2);
 
         //리스트 조회 검증
-        List<Member> all = memberJpaRepository.findAll();
+        List<Member> all = memberRepository.findAll();
         Assertions.assertThat(all.size()).isEqualTo(2);
 
         //카운트 검증
-        long count = memberJpaRepository.count();
+        long count = memberRepository.count();
         Assertions.assertThat(count).isEqualTo(2);
 
         //삭제 검증
-        memberJpaRepository.delete(member1);
-        memberJpaRepository.delete(member2);
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
 
-        long deleteCount = memberJpaRepository.count();
+        long deleteCount = memberRepository.count();
         Assertions.assertThat(deleteCount).isEqualTo(0);
 
     }}
